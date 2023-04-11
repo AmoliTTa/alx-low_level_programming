9838e34cc6f60ba4aa6f07e087e6afc5949bae1f
Amal HAMMANI <amal.hammani2012@gmail.com>
2023-04-11 16:54:58 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,17 +13,25 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+	
 	if (s2 == NULL)
 		s2 = "";
-		i = x = 0;
+
+	i = x = 0;
+
 	while (s1[i] != '\0')
 		i++;
+
 	while (s2[x] != '\0')
 		x++;
+
 	conct = malloc(sizeof(char) * (i + x + 1));
+
 	if (conct == NULL)
 		return (NULL);
+
 	i = x = 0;
+
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
